078f673097d7ef6bc5de24289a747c69af00e115
t-dlsantos <thiagodlsantos9@gmail.com>
2024-01-07 15:15:45 -0300

    fix: fix some erros with the player object

--- main.js
+++ main.js
@@ -1,141 +1,139 @@
 const gameBoard = (() => {
-    const board = [];
-    const rows = 3;
-    const columns = 3;
+  const board = [];
+  const rows = 3;
+  const columns = 3;
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-          board[i].push(Cell());
-        }
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(createCell());
     }
+  }
 
-    const getBoard = () => board;
+  const getBoard = () => board;
 
-    const setPosition = (symbol, column, row) => {
-        const cell = board[row][column];
-    
-        if (cell.getValue() === '') {
-            cell.addSymbol(symbol); 
-            return true;
-        } else {
-            console.log(`Position (${row}, ${column}) is already occupied.`);
-            game.printNewRound();
-            return false;
-        }
-    };
+  const setPosition = (symbol, column, row) => {
+    const cell = board[row][column];
 
-    const printBoard = () => {
-      const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
-      console.log(boardWithCellValues);  
-    };
-    
-    return { getBoard, setPosition, printBoard };
+    if (cell.getValue() === '') {
+        cell.addSymbol(symbol); 
+        return true;
+    } else {
+        console.log(`Position (${row}, ${column}) is already occupied.`);
+        game.printNewRound();
+        return false;
+    }
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+    console.log(boardWithCellValues);  
+  };
+  
+  return { getBoard, setPosition, printBoard };
 })();
 
-function Cell() {
-    let value = '';
-  
-    const addSymbol = (symbol) => {
-      value = symbol;
-    };
-  
-    const getValue = () => value;
-  
-    return {
-      addSymbol,
-      getValue
-    };
+function createCell() {
+  let value = '';
+
+  const addSymbol = (symbol) => {
+    value = symbol;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addSymbol,
+    getValue
+  };
 }
 
-const game = ((
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-  ) => {
-  
-    const players = [
-      {
-        name: playerOneName,
-        symbol: 'X'
-      },
-      {
-        name: playerTwoName,
-        symbol: 'O'
-      }
-    ];
-  
-    let activePlayer = players[0];
-  
-    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
+function createPlayer(name, symbol) {
+  return {
+    name,
+    symbol
+  };
+}
 
-    const getActivePlayer = () => activePlayer;
-  
-    const printNewRound = () => {
-      gameBoard.printBoard();
-      console.log(`${getActivePlayer().name}'s turn.`);
-    };
-  
-    const playRound = (column, row) => {
-      console.log(
-        `Dropping ${getActivePlayer().name}'s symbol into column ${column}...`
-      );
+const game = (() => {
+  const players = [
+    createPlayer('Thiago', 'X'),
+    createPlayer('João', 'O')
+  ];
 
-      const setPostionStatus = gameBoard.setPosition(getActivePlayer().symbol, column, row);
-      
-      if (checkWinner()) {
-        printWinner();
-        return;
-      }
+  let activePlayer = players[0];
 
-      if (setPostionStatus) {
-        switchPlayerTurn();
-        printNewRound();  
-      } else {
-        return;
-      }
-    };
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    gameBoard.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (column, row) => {
+    console.log(
+      `Dropping ${getActivePlayer().name}'s symbol into column ${column}...`
+    );
+
+    const setPostionStatus = gameBoard.setPosition(getActivePlayer().symbol, column, row);
     
-    const printWinner = () => {
-        console.log(`${getActivePlayer().name} wins!`);
-    };
+    if (checkWinner()) {
+      printWinner();
+      return;
+    }
 
-    const checkWinner = () => {
-        const board = gameBoard.getBoard();
+    if (setPostionStatus) {
+      switchPlayerTurn();
+      printNewRound();  
+    } else {
+      return;
+    }
+  };
+  
+  const printWinner = () => {
+    console.log(`${getActivePlayer().name} wins!`);
+    gameBoard.printBoard();
+  };
 
-        for (let i = 0; i < 3; i++) {
-            if (
-              (board[i][0].getValue() !== '' &&
-                board[i][0].getValue() === board[i][1].getValue() &&
-                board[i][1].getValue() === board[i][2].getValue()) ||
-              (board[0][i].getValue() !== '' &&
-                board[0][i].getValue() === board[1][i].getValue() &&
-                board[1][i].getValue() === board[2][i].getValue())
-            ) {
-              return true;
-            }
-          }
-        
-          if (
-            (board[0][0].getValue() !== '' &&
-              board[0][0].getValue() === board[1][1].getValue() &&
-              board[1][1].getValue() === board[2][2].getValue()) ||
-            (board[0][2].getValue() !== '' &&
-              board[0][2].getValue() === board[1][1].getValue() &&
-              board[1][1].getValue() === board[2][0].getValue())
-          ) {
-            return true;
-          }
-        
-          return false;
-    };
-  
-    printNewRound();
-  
-    return {
-      playRound,
-      getActivePlayer,
-      printNewRound
-    };
+  const checkWinner = () => {
+    const board = gameBoard.getBoard();
+
+    for (let i = 0; i < 3; i++) {
+        if (
+          (board[i][0].getValue() !== '' &&
+            board[i][0].getValue() === board[i][1].getValue() &&
+            board[i][1].getValue() === board[i][2].getValue()) ||
+          (board[0][i].getValue() !== '' &&
+            board[0][i].getValue() === board[1][i].getValue() &&
+            board[1][i].getValue() === board[2][i].getValue())
+        ) {
+          return true;
+        }
+      }
+    
+      if (
+        (board[0][0].getValue() !== '' &&
+          board[0][0].getValue() === board[1][1].getValue() &&
+          board[1][1].getValue() === board[2][2].getValue()) ||
+        (board[0][2].getValue() !== '' &&
+          board[0][2].getValue() === board[1][1].getValue() &&
+          board[1][1].getValue() === board[2][0].getValue())
+      ) {
+        return true;
+      }
+    
+      return false;
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+    printNewRound,
+  };
 })();
